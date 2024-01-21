185df95b3de49ddbdce95d0132e1d739e0d6ec2f
Ira Kurthen <ira.kurthen@hotmail.com>
2024-01-21 16:08:32 +0100

    rename ui to scoredisplay

--- scripts/main.gd
+++ scripts/main.gd
@@ -6,7 +6,7 @@ var enemy_points = 0
 @onready var player_paddle = $PlayerPaddle
 @onready var enemy_paddle = $EnemyPaddle
 @onready var ball = $Ball
-@onready var ui = $UI
+@onready var score_display = $ScoreDisplay
 
 
 func _ready():
@@ -21,22 +21,22 @@ func _process(_delta):
 func new_game():
 	player_points = 0
 	enemy_points = 0
-	ui.update_player_score(player_points)
-	ui.update_enemy_score(enemy_points)
-	reset_game_state()
-
-
-func _on_enemy_point_scored():
-	enemy_points += 1
-	print_debug("Enemy points: " + str(enemy_points))
-	ui.update_enemy_score(enemy_points)
+	score_display.update_player_score(player_points)
+	score_display.update_enemy_score(enemy_points)
 	reset_game_state()
 
 
 func _on_player_point_scored():
 	player_points += 1
 	print_debug("Player points: " + str(player_points))
-	ui.update_player_score(player_points)
+	score_display.update_player_score(player_points)
+	reset_game_state()
+
+
+func _on_enemy_point_scored():
+	enemy_points += 1
+	print_debug("Enemy points: " + str(enemy_points))
+	score_display.update_enemy_score(enemy_points)
 	reset_game_state()
 
 
